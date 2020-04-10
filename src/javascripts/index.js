1e2ed0841ebead706a54e3deef04ff829155ed71
Hannah Jex <hannahjex1@mail.weber.edu>
2020-04-10 09:31:51 -0600

    needs functionality

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,16 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import 'bootstrap'
+
+localStorage.setItem('sample item', 120)
+
+document.querySelector('#newCard').onClick = function(){
+    document.querySelector('#myForm').classList.remove('d-none')
+    document.querySelector('#cards').classList.add('d-none')
+}
+
+document.form[1].querySelector('#newCard').onClick = function(){
+    document.querySelector('#myForm').classList.remove('d-none')
+    document.querySelector('#cards').classList.add('d-none')
+}
